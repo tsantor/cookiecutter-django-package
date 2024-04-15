eb41fa97ee4842ba839bedd57d7ed1677f9baab3
Tim Santor <tsantor@xstudios.com>
2024-04-15 10:56:00 -0400

    update

--- {{cookiecutter.package_name}}/django_project/urls.py
+++ {{cookiecutter.package_name}}/django_project/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 admin.site.enable_nav_sidebar = False
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("{{cookiecutter.package_name}}/", include("{{cookiecutter.project_slug}}.urls", namespace="{{cookiecutter.package_name}}")),
+    # path("admin/", admin.site.urls),
+    # path("{{cookiecutter.package_name}}/", include("{{cookiecutter.project_slug}}.urls", namespace="{{cookiecutter.package_name}}")),
 ]
